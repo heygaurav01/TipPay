5156130765709844ea13e08184450bc16fe316d2
unknown <sharmagaurav.aice@gmail.com>
2025-02-24 23:07:40 +0530

    save local changes

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express, { urlencoded } from 'express';
+import cors from 'cors';  // Import CORS
 import { config } from 'dotenv';
 import employeeRoute from './src/v1/routes/employee.route.js';
 import connection from './src/v1/config/connection.config.js';
 import paymentRoute from './src/v1/routes/payment.route.js';
 import payoutsRoute from './src/v1/routes/payouts.route.js';
-import employerRoute from './src/v1/routes/employer.route.js';  //  New Import
+import employerRoute from './src/v1/routes/employer.route.js';  // New Import
 
 config();
 connection();
 const app = express();
 const port = process.env.PORT || 5009;
 
+// Enable CORS for all IPs
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
-app.use(express.json({ urlencoded: true }));
+app.use(express.urlencoded({ extended: true }));  // Fix urlencoded issue
 
 employeeRoute(app);
 paymentRoute(app);
 payoutsRoute(app);
-employerRoute(app);  //  Add employer routes
+employerRoute(app);  // Add employer routes
 
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
